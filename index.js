360cd4473faa410c70fd667d97d4ba776dc1dd93
wellyngthonsanveira <wellyngthonsanveira@gmail.com>
2025-10-09 13:23:57 -0300

    NewWindow

---
+++ index.js
@@ -0,0 +1,14 @@
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
